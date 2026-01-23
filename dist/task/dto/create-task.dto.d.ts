067f0a0b24100b5a9502cf3d0e333fd4fa039d22
Kseniia_Liepokhina <kseniya.lepiohina@gmail.com>
2026-01-23 18:20:34 +0200

    added a swagger docs

---
+++ dist/task/dto/create-task.dto.d.ts
@@ -0,0 +1,5 @@
+export declare class CreateTaskDto {
+    userId?: number;
+    title?: string;
+    description?: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
     methods:'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  const config = new DocumentBuilder()
+  .setTitle('ToDoList')
+  .setDescription('The todolist API Description')
+  .setVersion('1.0')
+  .addTag('to do')
+  .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
