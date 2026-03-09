13257ac50d04160118b45494bc9c2a9fb8a62595
Kseniia <kseniya.lepiohina@gmail.com>
2026-03-09 15:43:55 +0200

    chore: remove dist and node_modules from tracking

--- dist/auth/entities/auth.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { ActiveTasks } from 'src/task/entities/ActiveTasks.entity';
-import { CompletedTasks } from 'src/task/entities/CompletedTasks.entity';
-export declare class Auth {
-    id: number;
-    username: string;
-    password: string;
-    activeTasks: ActiveTasks[];
-    completedTasks: CompletedTasks[];
-}
